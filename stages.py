bfda62e64c279a8381e5c54e0c5c30b3e503ef1f
Katherine Laliotis <67384941+kklaliotis@users.noreply.github.com>
2026-02-04 13:45:35 -0500

    Outline Destripe Stage

--- stages.py
+++ stages.py
@@ -1,17 +1,20 @@
 from ceci import PipelineStage
-from scm_pipeline import ASDFFile, ParquetFile
+from scm_pipeline import ASDFFile, TextFile, Directory
 from .types import TextFile, YamlFile
 
 
-class DetectionStage(PipelineStage):
+class Destripe(PipelineStage):
     """
-    This pipeline element is for detecting objects via SEP
+    This pipeline element is for removing noise stripes from mosaic images.
     """
 
-    name = "DetectionStage"
-    inputs = [("Exposure", ASDFFile)]
-    outputs = [("object_catalog", ParquetFile)]
-    config_options = {"detection_threshold": float}
+    name = "Destripe"
+    # Inputs to this stage. Format: List of (name, datatype) (see: scm_pipeline/data_types)
+    inputs = [("images", Directory), ("manifest_file", TextFile)]
+    # Outputs from this stage. Format: List of (name, datatype)
+    outputs = [("destriped_images", Directory), ("manifest_file", TextFile)]  # KL Maybe we want to update the manifest file to exclude destriping anomaly images from coadds?
+    # Config options -- these should match the config options for this stage in config.yaml. Format: {name: dtype}
+    config_options = {"something": float}
 
     def run(self):
         # Retrieve configuration:
@@ -19,21 +22,24 @@ class DetectionStage(PipelineStage):
         print("Here is my configuration :", my_config)
 
        
-        filename = self.get_input("Exposure")
-        print(f" DetectionStage reading from {filename}")
+        path_to_images = self.get_input("images")
+        manifest_file = self.get_input("manifest_file")
+        print(f" Destripe Stage reading images from {path_to_images} according to manifest file {manifest_file}")
         blah = process(filename)
 
-        filename = self.get_output("object_catalog")
-        print(f" DetectionStage writing to {filename}")
+        path_to_images = self.get_output("destriped_images")
+        manifest_file = self.get_output("manifest_file")
+        print(f" Destripe Stage writing destriped images to to {path_to_images}")
+        print(f"Manifest file {manifest_file} updated.")
         open(filename, "w").write(blah)
 
-class SelectionStage(PipelineStage):
+class PSFSplit(PipelineStage):
     """
     This pipeline element is for selecting stars for psf fitting
     """
 
     name = "SelectionStage"
-    inputs = [("object_catalog", ParquetFile)]
+    inputs = [("destriped_images", Directory)]
     outputs = [("star_catalog", ParquetFile)]
     config_options = {"magnitude_cut": float}
 
